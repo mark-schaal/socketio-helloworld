98481fd94fc6d112437ea46596cc8ee5b1ac2015
mark-schaal <markschaal@gmail.com>
2014-09-04 12:59:23 -0400

    Added filesystem and request capabilities to test external API connection capabilities and filestreaming

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
+var request = require('request');
 
 app.get('/', function(req, res){
-     var options = {
-        root: __dirname,
-     };
-     res.sendFile('index.html',options);
+	var options = {
+	root: __dirname,
+	};
+	res.sendFile('index.html',options);
+	request('https://data.usajobs.gov/api/jobs?series=2210').pipe(fs.createWriteStream('test.json'));
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect',function(){
-        console.log('user disconnected');
-    });
-    socket.on('chat message',function(msg){
-	console.log('message: '+msg);
-    });
+	console.log('User socket connection detected.');
+	socket.on('disconnect',function(){
+		console.log('User socket disconnection detected.');
+	});
+	socket.on('chat message',function(msg){
+		console.log('Message processed: '+msg);
+		io.emit('chat message',msg);
+	});
 });
 
 http.listen(3000, function(){
-    console.log('listening on *:3000');
+	console.log('Server instantiated and listening on *:3000');
 });
