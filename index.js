38a36c8b5b61139bf7e77266a1eaa7fb7d38525b
mark-schaal <markschaal@gmail.com>
2014-09-04 12:05:42 -0400

    Added ability to emit events by passing data through the client library back to socket.io

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ io.on('connection', function(socket){
     socket.on('disconnect',function(){
         console.log('user disconnected');
     });
+    socket.on('chat message',function(msg){
+	console.log('message: '+msg);
+    });
 });
 
 http.listen(3000, function(){
