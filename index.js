ef48a7e212a787ba6f68a8b305b5e4b6d523378f
mark-schaal <markschaal@gmail.com>
2014-09-04 11:58:12 -0400

    Add disconnect behavior for socket connection

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+    console.log('a user connected');
+    socket.on('disconnect',function(){
+        console.log('user disconnected');
+    });
 });
 
 http.listen(3000, function(){
